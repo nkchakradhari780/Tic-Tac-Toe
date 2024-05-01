b4ae93c3729909da37c38021f5c10e60c74fcb63
nkchakradhari780 <154994765+nkchakradhari780@users.noreply.github.com>
2024-05-01 22:14:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ let NewGame = document.querySelector("#newgame");
 let xScore = document.querySelector("#xscore");
 let tieScore = document.querySelector("#tiescore");
 let oScore = document.querySelector("#oscore");
+let strike = document.querySelector("#strike");
 let btnclick=0;
+let changeMode = document.querySelector("#mode");
 const winningState = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    {combo:[0,1,2], strikeClass: "line1"},
+    {combo:[3,4,5], strikeClass: "line2"},
+    {combo:[6,7,8], strikeClass: "line3"},
+    {combo:[0,3,6], strikeClass: "line4"},
+    {combo:[1,4,7], strikeClass: "line5"},
+    {combo:[2,5,8], strikeClass: "line6"},
+    {combo:[0,4,8], strikeClass: "line7"},
+    {combo:[2,4,6], strikeClass: "line8"},
 ];
 let px = 0;
 let po = 0;
 let tie = 0;
 ResetGame.addEventListener("click", () => {
     console.log("Reset button clicked");
+    for(let pattern of winningState){
+        const {combo,strikeClass} = pattern;
+        strike.classList.remove(strikeClass);
+    }
     winbox.style.display = "none"; // Hide the winbox div
     enableBox();
     box.innerText = "";
@@ -31,6 +37,10 @@ ResetGame.addEventListener("click", () => {
 NewGame.addEventListener("click", ()=> {
     console.log("New Game button clicked");
     winbox.style.display = "none";
+    for(let pattern of winningState){
+        const {combo,strikeClass} = pattern;
+        strike.classList.remove(strikeClass);
+    }
     px = 0;
     po = 0;
     tie = 0;
@@ -65,12 +75,15 @@ const disableBox = () => {
 }
 const checkWinner = () => {
     for(let pattern of winningState){
-        let pos1 = boxes[pattern[0]].innerText;
-        let pos2 = boxes[pattern[1]].innerText;
-        let pos3 = boxes[pattern[2]].innerText;
+        const {combo,strikeClass} = pattern;
+        let pos1 = boxes[combo[0]].innerText;
+        let pos2 = boxes[combo[1]].innerText;
+        let pos3 = boxes[combo[2]].innerText;
         if(pos1 != "" && pos2 != "" && pos3 != ""){
             if(pos1 === pos2 && pos2 === pos3){
                 console.log("winnner");
+                strike.style.display = "block";
+                strike.classList.add(strikeClass);
                 winbox.style.display = "block";
                 if(playerX === false){
                     winbox.innerText = "Winner is X"
